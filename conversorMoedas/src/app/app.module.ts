b5f4132ef728bc34691ca073c1d2b5b48e9fcfd9
jvoliveirag <jv.oliveira@gmail.com>
2022-12-01 01:41:05 -0300

    Criação de diretórios específicos para cada parte do código e criação de método para converter de Real para Dolar

--- conversorMoedas/src/app/app.module.ts
+++ conversorMoedas/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'; //responsavel pela requisicao na API
 import { CommonModule } from '@angular/common';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app.component';
 
 @NgModule({
   declarations: [
--- conversorMoedas/src/app/components/app.component.ts
+++ conversorMoedas/src/app/components/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { ConversorService } from './conversor.service';
+import { ConversorService } from '../services/conversor.service';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
   title = 'conversorMoedas';
@@ -13,9 +13,13 @@ export class AppComponent {
   valorConvertido: number;
   visibilidadeValorConvertido: boolean;
 
+  visibilidadeValorConvertidoDolar: boolean;
+  valorConvertido_dolar:number;
+
   constructor(private conversorService: ConversorService){ }
 
   ngOnInit(): void {
+    this.visibilidadeValorConvertidoDolar = false; //comeca sem ser mostrado
     this.visibilidadeValorConvertido = false; //comeca sem ser mostrado
     this.formulario = new FormGroup({
       valor: new FormControl(null)
@@ -27,9 +31,22 @@ export class AppComponent {
     const valor = this.formulario.value.valor; //valor inserido no form
     this.visibilidadeValorConvertido = true; //mostrar o valor convertido
 
+    //Real -> Dolar
     this.conversorService.RealizarConversao().subscribe(resultado => { //subscribe = executar (metodo do Observable)
-      console.log(resultado);
+      //console.log(resultado);
       this.valorConvertido = Number((valor * resultado.rates.BRL).toFixed(2));
+    })
+  }
+
+  ConverterDolar(): void {
+
+    const valor = this.formulario.value.valor; //valor inserido no form
+    this.visibilidadeValorConvertidoDolar = true; //mostrar o valor convertido
+
+    //Dolar -> Real
+    this.conversorService.RealizarConversao().subscribe(resultado => { 
+      this.valorConvertido_dolar = Number((valor / resultado.rates.BRL).toFixed(2));
+      console.log(this.valorConvertido_dolar);
     })
   }
 }
